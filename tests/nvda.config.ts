33fdfd1a71d3a53616e1dc5be01b4d65322b0ddb
Cameron Cundiff <cameroncundiff@asana.com>
2023-10-14 11:27:05 -0400

    Add recordings

--- tests/nvda.config.ts
+++ tests/nvda.config.ts
@@ -1,5 +1,6 @@
-import { nvda } from "@guidepup/guidepup";
+import { nvda, windowsActivate, windowsRecord } from "@guidepup/guidepup";
 import { test as base } from "@playwright/test";
+import { join } from "path";
 
 export type TestOptions = {
     nvda: typeof nvda;
@@ -7,12 +8,20 @@ export type TestOptions = {
 
 export const test = base.extend<TestOptions>({
     nvda: async ({}, use) => {
+        let stopRecording:() => void;
+
         try {
+            const chromePath = join(__dirname, "../node_modules/playwright-core/.local-browsers/chromium-1080/chrome-win/chrome.exe");
+            const recordingPath = join(__dirname, `../recordings/playwright-nvda-firefox-${+new Date()}.mp4`);
             await nvda.start();
+            await windowsActivate(chromePath, "Chromium");
+            stopRecording = windowsRecord(recordingPath);
             await use(nvda);
         } finally {
+            stopRecording();
             await nvda.stop();
         }
+
     }
 });
 
